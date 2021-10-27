a08c68f75d2a1c76c8f6c1dff341e8ac1a251336
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-10-27 15:11:14 +0200

    Change Github Login Service

--- backend/src/main/java/de/neuefische/devquiz/security/service/GitHubLoginService.java
+++ backend/src/main/java/de/neuefische/devquiz/security/service/GitHubLoginService.java
@@ -4,13 +4,18 @@ import de.neuefische.devquiz.security.api.GitHubApiService;
 import de.neuefische.devquiz.security.model.GitHubUserDto;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
+
 @Service
 public class GitHubLoginService {
 
     private final GitHubApiService gitHubApiService;
+    private final JWTUtilService jwtUtilService;
 
-    public GitHubLoginService(GitHubApiService gitHubApiService) {
+    public GitHubLoginService(GitHubApiService gitHubApiService, JWTUtilService jwtUtilService) {
         this.gitHubApiService = gitHubApiService;
+        this.jwtUtilService = jwtUtilService;
     }
 
     public String verifyGitHubCode(String code) {
@@ -19,9 +24,8 @@ public class GitHubLoginService {
 
         // 2) Retrieve User Info
         GitHubUserDto gitHubUserDto = gitHubApiService.retrieveUserInfo(gitHubToken);
-        // 3) Create JWT access token
 
-        // TODO
-        return "JWT!";
+        // 3) Create JWT access token
+        return jwtUtilService.createToken(new HashMap<>(), gitHubUserDto.getLogin());
     }
 }
